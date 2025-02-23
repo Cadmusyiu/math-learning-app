ef5f54b8b00c745ddb208e86eb301c01cd6275d3
Cadmusyiu <chunchunyiu0624@gmail.com>
2025-02-23 23:32:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import MathLearningApp from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MathLearningApp />
+  </React.StrictMode>
+);
